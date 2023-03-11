7e64b64e51785ae8dc76be1d31c8673f8a6e4033
ilieBeracha <ilieberacha123@gmail.com>
2023-03-11 21:20:17 +0200

    v

--- src/Components/WelcomeComponentMobile/PhoneNotify/PhoneNotify.jsx
+++ src/Components/WelcomeComponentMobile/PhoneNotify/PhoneNotify.jsx
@@ -52,7 +52,7 @@ export default function PhoneNotify() {
         <Box sx={style}>
           <div className="PhoneNotifyModal">
             {/* <h2>Submit: </h2> <hr /> */}
-            <input pattern="/^[^\s@]+@[^\s@]+\.[^\s@]+$/" onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" />
+            <input tabIndex="-1" pattern="/^[^\s@]+@[^\s@]+\.[^\s@]+$/" onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" />
             <button onClick={saveEmail}>Submit</button>
           </div>
         </Box>
