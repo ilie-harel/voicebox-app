56fdf28771bd625c337e9a2d3f2cca3bbb5ab743
ilieBeracha <ilieberacha123@gmail.com>
2023-03-12 13:53:03 +0200

    aa

--- src/Components/Main/Home/Home.jsx
+++ src/Components/Main/Home/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
   }, [])
 
   return (
-    <div className={notComputer? 'Home HomeMobile' : 'Home'}>
+    <div className={notComputer? 'Home ComponentMobile' : 'Home'}>
       {
         smallScreen ? 
         <RoomsSmallScreen />
