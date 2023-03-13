2bec867c116c56b37028088ee18d6e4f4deef2ff
ilieBeracha <ilieberacha123@gmail.com>
2023-03-13 14:16:45 +0200

    d

--- src/Components/WelcomeComponentMobile/WelcomeComponentMobile.jsx
+++ src/Components/WelcomeComponentMobile/WelcomeComponentMobile.jsx
@@ -16,7 +16,7 @@ export default function WelcomeComponentMobile() {
       </div>
       <div className='WelcomeComponentMobilePhone'>
         <TypeAnimation
-          sequence={['We are currently working on bringing Voice Box AI to your phone and Safari browser. In the meantime, you can try it out on your computer.']}
+          sequence={['We are currently working on bringing Voice Box AI to your phone and Safari browser. In the meantime, you can try it out from any other platform.']}
           wrapper="p"
           cursor={true}
           speed={50}
